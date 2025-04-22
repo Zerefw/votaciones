d6123f3c16f23b431599707aee5330cb6cd57970
Zerefw <166086021+Zerefw@users.noreply.github.com>
2025-04-22 02:24:02 -0500

    Actualizar rutas de candidatos: agregar manejo de errores, refactorizar funciones y establecer prefijo en el enrutador

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.routes.vote_route import router as vote_router
 
 app = FastAPI(title="API de votaciones", description="API para contabilizar votos de los candidatos que se registren", version="1.0")
 app.include_router(voter_router)
-app.include_router(candidate_router, prefix="/candidate", tags=["Candidatos"])
+app.include_router(candidate_router)
 app.include_router(vote_router, prefix="/votes", tags=["Votos"])
 
 @app.get("/")
--- app/routes/candidate_route.py
+++ app/routes/candidate_route.py
@@ -1,20 +1,34 @@
-from fastapi import APIRouter
-from app.schemas.candidate_schema import CandidateCreate
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models.candidate_model import Candidate
+from app.schemas.candidate_schema import CandidateCreate, CandidateOut
 
-router = APIRouter()
+router = APIRouter(prefix="/candidates", tags=["Candidatos"])
 
-@router.post("")
-async def create_candidate(candidate_data: CandidateCreate):
-  return ''
+@router.post("/", response_model=CandidateOut, status_code=status.HTTP_201_CREATED)
+def create_candidate(candidate: CandidateCreate, db: Session = Depends(get_db)):
+    new_candidate = Candidate(**candidate.dict())
+    db.add(new_candidate)
+    db.commit()
+    db.refresh(new_candidate)
+    return new_candidate
 
-@router.get("")
-async def get_all_candidates():
-  return ''
+@router.get("/", response_model=list[CandidateOut])
+def get_candidates(db: Session = Depends(get_db)):
+    return db.query(Candidate).all()
 
-@router.get("/{id}")
-async def get_candidate_by_id(id: int):
-  return ''
+@router.get("/{candidate_id}", response_model=CandidateOut)
+def get_candidate(candidate_id: int, db: Session = Depends(get_db)):
+    candidate = db.query(Candidate).filter(Candidate.id == candidate_id).first()
+    if candidate is None:
+        raise HTTPException(status_code=404, detail="Candidato no encontrado")
+    return candidate
 
-@router.delete("/{id}")
-async def delete_candidate_by_id(id: int):
-  return ''
+@router.delete("/{candidate_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_candidate(candidate_id: int, db: Session = Depends(get_db)):
+    candidate = db.query(Candidate).filter(Candidate.id == candidate_id).first()
+    if candidate is None:
+        raise HTTPException(status_code=404, detail="Candidato no encontrado")
+    db.delete(candidate)
+    db.commit()
